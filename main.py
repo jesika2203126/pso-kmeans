e1b05a0519ed2657cc72f611802f04346c30ce22
jesika2203126 <jesika.yudiani.2203126@students.um.ac.id>
2026-06-26 01:35:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,26 @@ mean      = np.array(params["mean"])
 std       = np.array(params["std"])
 centroids = np.array(params["centroids_zscore"])
 
+LABEL_MAP = {
+    1: "Severe Insulin-Deficient Diabetes (SIDD)",
+    2: "Mild Obesity-Related Diabetes (MOD)", 
+    3: "Severe Insulin-Resistant Diabetes (SIRD)",
+    4: "Mild Age-Related Diabetes (MARD)"
+}
+
 class InputData(BaseModel):
     Usia: float
     BMI: float
     HbA1c: float
-    HOMA2_B: float
-    HOMA2_IR: float
+    HOMA2B: float   # ← sesuaikan dengan yang Laravel kirim
+    HOMA2IR: float  # ← sesuaikan dengan yang Laravel kirim
 
 @app.post("/predict")
 def predict(data: InputData):
-    x = np.array([data.Usia, data.BMI, data.HbA1c, data.HOMA2_B, data.HOMA2_IR])
+    x = np.array([data.Usia, data.BMI, data.HbA1c, data.HOMA2B, data.HOMA2IR])
     x_scaled = (x - mean) / std
     klaster  = int(np.argmin(np.linalg.norm(centroids - x_scaled, axis=1))) + 1
-    return {"klaster": klaster}
+    return {
+        "klaster": klaster,
+        "label": LABEL_MAP[klaster]  # ← tambahkan ini
+    }
